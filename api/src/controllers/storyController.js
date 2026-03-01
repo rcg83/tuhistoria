3a0a3931194529e66ce22c52df85e9708260b80f
rcg83 <rmncsp@gmail.com>
2026-03-01 22:38:37 +0100

    feat: preparación del endpoint api/stories/create para que el usuario inicie historias

--- api/src/controllers/storyController.js
+++ api/src/controllers/storyController.js
@@ -1,4 +1,5 @@
-import Story from '../schemes/Story.js';
+import StoryTemplate from '../schemes/StoryTemplate.js';
+import StoryInstance from '../schemes/StoryInstance.js';
 
 export const createStoryTemplate = async (req, res) => {
   try {
@@ -8,7 +9,7 @@ export const createStoryTemplate = async (req, res) => {
       return res.status(400).json({ message: 'El título es obligatorio' });
     }
 
-    const newStory = await Story.create({ title, content });
+    const newStory = await StoryTemplate.create({ title, content });
     res.status(201).json({
       message: 'Historia creada correctamente',
       storyId: newStory._id
@@ -17,3 +18,31 @@ export const createStoryTemplate = async (req, res) => {
     res.status(500).json({ message: 'Error al crear la historia' });
   }
 };
+
+export const startStory = async (req, res) => {
+  try {
+    const { templateId } = req.body;
+    if (!templateId) {
+      return res.status(400).json({ message: 'Se requiere templateId' });
+    }
+
+    const template = await StoryTemplate.findById(templateId);
+    if (!template) {
+      return res.status(404).json({ message: 'Template de la historia no encontrado' });
+    }
+
+    const storyInstance = await StoryInstance.create({
+      template: template._id,
+      user: req.user.id,
+      messages: []
+    });
+
+    res.status(201).json({
+      message: 'Historia iniciada correctamente',
+      storyInstanceId: storyInstance._id
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: 'Error al iniciar la historia' });
+  }
+}
--- api/src/routes/storyRoutes.js
+++ api/src/routes/storyRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createStoryTemplate } from '../controllers/storyController.js';
+import { createStoryTemplate, startStory } from '../controllers/storyController.js';
 import { protect, authorize } from '../middlewares/authMiddlewares.js';
 
 const router = express.Router();
 
 router.post('/', protect, authorize('admin'), createStoryTemplate);
+router.post('/start', protect, startStory);
 
 export default router;
