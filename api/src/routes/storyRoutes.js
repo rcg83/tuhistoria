154beb1f4b8af2ace0fe83167247257423cd7278
rcg83 <rmncsp@gmail.com>
2026-03-02 18:18:45 +0100

    feat: preparado endpoint para testear que la key de Gemini funciona

--- api/src/routes/storyRoutes.js
+++ api/src/routes/storyRoutes.js
@@ -2,6 +2,9 @@ import express from 'express';
 import { createStoryTemplate, startStory, getStories } from '../controllers/storyController.js';
 import { protect, authorize } from '../middlewares/authMiddlewares.js';
 
+import { testGemini } from '../services/geminiService.js';
+
+
 const router = express.Router();
 
 router.post('/', protect, authorize('admin'), createStoryTemplate);
@@ -9,5 +12,24 @@ router.post('/start', protect, startStory);
 
 /* RUTAS PROTEGIDAS a nivel rol "admin" */
 router.get('/', protect, authorize('admin'), getStories);
+
+
+
+router.get('/test-ai', async (req, res) => {
+  try {
+
+    const respuesta = await testGemini("Responde con algo breve e interesante sobre programación.!'");
+    res.json({ 
+      status: "Conexión con Gemini establecida", 
+      respuesta 
+    });
+  } catch (error) {
+    res.status(500).json({ 
+      error: "Error en el SDK de Google", 
+      mensaje: error.message 
+    });
+  }
+});
+
 
 export default router;
--- api/src/services/geminiService.js
+++ api/src/services/geminiService.js
@@ -1,11 +1,15 @@
 /* Servicio encargado de la comunicación directa con la API de Google Gemini. */
 
 import { GoogleGenerativeAI } from "@google/generative-ai";
+import 'dotenv/config';
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 export const testGemini = async (prompt) => {
+  const model = genAI.getGenerativeModel({
+    model: "gemini-flash-latest"
+  });
+
   const result = await model.generateContent(prompt);
   return result.response.text();
 };
