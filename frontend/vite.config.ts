b263b19055c66e742d113a730d19377f63fd030c
rcg83 <rmncsp@gmail.com>
2026-06-08 14:19:37 +0200

    fix: Dockerfile en Vite ajustado para evitar error de parseo

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,6 +12,9 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: 5173,
+    fs: {
+      deny: ['Dockerfile'],
+    },
     watch: {
       usePolling: true,
       interval: 100,
